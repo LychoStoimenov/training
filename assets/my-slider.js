c477891c703748c222ed27108e8c7618e8455a90
Lachezar <lachezar@crispstudio.es>
2022-06-15 18:16:13 +0300

    Add Updates from ticket BGCMMSHPFY-1974

---
+++ assets/my-slider.js
@@ -0,0 +1,14 @@
+const sliders = document.querySelectorAll('.slider-default');
+
+sliders.forEach((slider) => {
+  const initSlider = slider.querySelector('.swiper');
+  const dots = slider.querySelector('.swiper-pagination');
+
+  const mySwiper = new Swiper(initSlider, {
+    loop: true,
+    pagination: {
+      el: dots,
+      clickable: true,
+    },
+  });
+});
