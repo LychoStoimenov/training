65a0a27de3c9c688306d464aa52809c445963279
Lachezar <lachezar@crispstudio.es>
2022-06-24 08:43:36 +0300

    ADD SLIDER IMAGES / VIDEOS

--- assets/my-slider.js
+++ assets/my-slider.js
@@ -12,3 +12,18 @@ sliders.forEach((slider) => {
     },
   });
 });
+
+const slidersVideo = document.querySelectorAll('.slider-image-video');
+
+slidersVideo.forEach((slider) => {
+  const initSlider = slider.querySelector('.swiper');
+  const dots = slider.querySelector('.swiper-pagination');
+
+  const mySwiper = new Swiper(initSlider, {
+    loop: true,
+    pagination: {
+      el: dots,
+      clickable: true,
+    },
+  });
+});
